18b23cb39ba700a0b7687914d34b2d5ce034dd25
anarchymonkey <Aniketbatabyal1996@gmail.com>
2021-04-11 14:28:52 +0530

    added chat frontend code

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'easy-peasy';
+
+import chat from './models/Chat';
+import login from './models/Login';
+
+
+const getStoreModels = () => {
+    return {
+        chat,
+        login
+    }
+};
+
+export const store = createStore(getStoreModels(), {
+    disableImmer: true,
+});
+
---
+++ src/widgets/InputBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+
+
+const InputBox = ({
+    label,
+    onChange,
+    ...restProps
+}) => {
+
+    return (
+        <TextField label={label} onChange={(ev) => onChange(ev)} {...restProps} />
+    )
+}
+
+export default InputBox;
